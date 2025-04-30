83221b0b44751c730f2ad7bce46688355e9c39e6
again-Suleman <contact.suleman46@gmail.com>
2025-04-30 12:26:23 +0500

    Server Settings

--- server/startup/route.js
+++ server/startup/route.js
@@ -1,8 +1,10 @@
 // Routes
 const postRouter = require("../routes/post")
+const dashboardRouter = require("../routes/dashboard")
 
 
 module.exports = function (app){
-    app.use('/api/post', postRouter)
+    app.use('/api/posts', postRouter)
+    app.use('/api/dashboard', dashboardRouter)
 
 }
